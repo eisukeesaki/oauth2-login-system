ae02a4973191e063aa87a872c3c7d1079aa69813
Eisuke Esaki <izkyyy@gmail.com>
2022-07-07 16:59:47 -0700

    log request header

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 4242;
 require("./src/db");
 
 app.get("/", (req, res) => {
+  l.info("detected request. request header:\n", req.headers);
   res.send("you have just made a GET request to /");
 });
 
