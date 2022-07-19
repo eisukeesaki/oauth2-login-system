52f7777bf0393a780d5b6aa0902f99a223348ac8
Eisuke Esaki <acekaydev@gmail.com>
2022-07-19 16:26:32 -0700

    implemented GET /api/maps. does not auth request

--- api/src/routes/maps.route.js
+++ api/src/routes/maps.route.js
@@ -1,12 +1,33 @@
+const { logger: l } = require("@utils/logger.util");
 const maps = require("express").Router();
+const db = require("@services/db.service");
 
-maps.get("/api/maps", (req, res) => {
-  // expects ?owner_id=id
-  console.log("req.query - ", req.query);
+maps.get("/api/maps", async (req, res) => {
+  try {
+    const userId = req.query.user_id;
+    const validate = require("uuid-validate");
+    const isValidQs = validate(userId);
 
-  // validate query param
-  // TODO
-  res.send(req.query);
+    if (isValidQs) {
+      const qs = "SELECT * FROM maps WHERE user_id = $1";
+      const qp = [userId];
+
+      const { rows } = await db.query(qs, qp);
+      l.debug({ rows: rows });
+
+      res.send(rows);
+    } else {
+      throw new Error("query string is not a valid UUID", { cause: "InvalidUUID" });
+    }
+  } catch (e) {
+    if (e.cause === "InvalidUUID") {
+      l.error({ message: e.message, cause: e.cause });
+      res.status(400).send(e.message);
+    } else {
+      l.error(e);
+      throw new Error("unhandled exception");
+    }
+  }
 });
 
 module.exports = maps;
