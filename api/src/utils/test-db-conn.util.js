1de71b7ea6fd0acc6ff524ee46f393a983d7f561
Eisuke Esaki <acekaydev@gmail.com>
2022-07-25 14:14:09 -0700

    minor fixes to testDbConn()

--- api/src/utils/test-db-conn.util.js
+++ api/src/utils/test-db-conn.util.js
@@ -1,11 +1,13 @@
-const { logger: l } = require("@utils/logger.util");
-
-async function testDbConn(pool) {
+module.exports = async function testDbConn(pool) {
   try {
-    l.info("querying database...")
-    const res = await pool.query("SELECT NOW()");
+    const { logger: l } = require("@utils/logger.util");
+    l.info("querying database...");
 
-    if (res.rows.length)
+    const res = await pool.query("SELECT NOW()");
+    // const { rows } = await pool.query("SELECT * FROM federated_credentials WHERE provider = 'https://accounts.google.com' AND subject = '101817407341620423232'");
+    // l.info("rows - ", rows);
+
+    if (res.rowCount)
       l.info("database query successful");
     else
       throw new Error("failed to do a test query at server start up");
@@ -13,6 +15,3 @@ async function testDbConn(pool) {
     l.info("failed to do a test query on database on server start up.\n%s", error.stack);
   }
 }
-
-module.exports = testDbConn;
-
