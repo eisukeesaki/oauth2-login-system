e3d5403d0c7dd0dd6c8daeeebd8bb45424b95a49
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-08-01 16:41:51 -0700

    uninstalled npm library from root

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    rootElement
 );
--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// import { GoogleLogin } from "react-google-login";
 
 const Signin = () => {
-    return <div>Signin</div>;
+    return (
+        <div>
+            hello
+            {/* <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+            /> */}
+        </div>
+    );
 };
 
 export default Signin;
