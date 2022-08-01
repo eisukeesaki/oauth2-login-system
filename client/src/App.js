ccb95a8e80b880c9c601e22571782f44e18dcda0
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-08-01 15:35:03 -0700

    reconstructed app conponent and added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React, { createRef, useEffect } from 'react';
-import renderMindMap from './Components/DrawMap/renderMindMap';
+import React, { createRef, useEffect } from "react";
+import renderMindMap from "./Components/DrawMap/renderMindMap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin/Signin";
 
 export default function App() {
-  const divRef = createRef();
-  useEffect(() => {
-    renderMindMap(divRef.current);
-  }, [divRef]);
-  return (
-    <div ref={divRef} />
-  );
+    const divRef = createRef();
+    useEffect(() => {
+        renderMindMap(divRef.current);
+    }, [divRef]);
+    return (
+        <>
+            {/* <div ref={divRef} /> */}
+            <BrowserRouter>
+                <Routes>
+                    {/* <Route path="/" element={<Home />} /> */}
+                    <Route path="/signin" element={<Signin />} />
+                    {/* <Route path="/mindlist" element={<MindList />} />
+                    <Route path="/signup" element={<Signup />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
