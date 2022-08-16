d33398ded69b3b65af4f62d27e235cc4b7768293
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-08-15 17:48:14 -0700

    added redirectUrl on GoogleLogin component

--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -4,14 +4,18 @@ import GoogleLogin from "react-google-login";
 const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Signin = () => {
+    const responseGoogle = (res) => {
+        console.log(res);
+    };
     return (
         <div>
             <GoogleLogin
                 clientId={googleClientId}
                 buttonText="Sign in with Google"
-                // onSuccess={responseGoogle}
-                // onFailure={responseGoogle}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
                 cookiePolicy={"single_host_origin"}
+                redirectUri={"http://localhost:3000"}
             />
         </div>
     );
